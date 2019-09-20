a5a7e58c677bb4eabaf8224fc2aa8bb5cb825957
kimjooan <jjaa2017@daum.net>
2019-09-20 19:28:20 +0900

    맥북-김주안
    - VR 모바일 (구글 카드보드 추가했음)
    - 문제 10문제 출제가 필요.

--- work/Assets/02_Script/CartManage.cs
+++ work/Assets/02_Script/CartManage.cs
@@ -9,6 +9,13 @@ public class CartManage : MonoBehaviour
     public float TrainPlayGoSpeedFloat;
 
     public float TrainSetfloatZ = -530f;
+
+
+    public float StopPositionZ;
+
+    public GameObject GameProblemPanel;
+
+
     void Start()
     {
 
@@ -20,8 +27,30 @@ public class CartManage : MonoBehaviour
     {
         if(TrainPlayBool==true)
         {
-            TrainSetfloatZ = TrainSetfloatZ + TrainPlayGoSpeedFloat;
-            this.gameObject.transform.localPosition = new Vector3(185.49f, 1.37f, TrainSetfloatZ);
+            if(TrainSetfloatZ >= StopPositionZ)
+            {
+                //게임 문제 패널 실행
+                GameProblemPanel.SetActive(true);
+
+                TrainPlayBool = false;
+            }
+            else
+            {
+                TrainSetfloatZ = TrainSetfloatZ + TrainPlayGoSpeedFloat;
+                this.gameObject.transform.localPosition = new Vector3(185.49f, 1.37f, TrainSetfloatZ);
+
+            }
+
         }
+    }
+
+
+    public void ReStartGame()
+    {
+        //게임 문제 패널 숨기기
+        GameProblemPanel.SetActive(false);
+        TrainSetfloatZ = -530f;
+        this.gameObject.transform.localPosition = new Vector3(185.49f, 1.37f, TrainSetfloatZ);
+        TrainPlayBool = true;
     }
 }
